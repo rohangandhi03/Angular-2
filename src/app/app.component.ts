9b00b650bdbda0674f936cd7654b9ea882030629
Rohan Gandhi <rohangandhi03@gmail.com>
2018-01-28 05:10:45 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import * as jquery from 'jquery';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './index.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  
+	 
+}
